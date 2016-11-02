81235ccbeece281ebce2b75673922975dfa501cd
bergeal1 <bergeal1@PROJEKTOR-J09.uhk.cz>
2016-11-02 14:59:26 +0100

    Back buttons

--- src/cz/uhk/fim/pro2/game/gui/GameScreen.java
+++ src/cz/uhk/fim/pro2/game/gui/GameScreen.java
@@ -1,5 +1,8 @@
 package cz.uhk.fim.pro2.game.gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 
 public class GameScreen extends Screen {
@@ -10,7 +13,12 @@ public class GameScreen extends Screen {
 		JButton jButtonBack = new JButton("BACK");
 		JButton jButtonPause = new JButton("PAUSE");
 		
-		
+		jButtonBack.addActionListener(new ActionListener() {			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				mainFrame.setScreen(new HomeScreen(mainFrame));
+			}
+		});
 		
 		add(jButtonBack);
 		add(jButtonPause);
--- src/cz/uhk/fim/pro2/game/gui/ScoreScreen.java
+++ src/cz/uhk/fim/pro2/game/gui/ScoreScreen.java
@@ -1,5 +1,8 @@
 package cz.uhk.fim.pro2.game.gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 
 public class ScoreScreen extends Screen {
@@ -8,6 +11,13 @@ public class ScoreScreen extends Screen {
 		super(mainFrame);
 		
 		JButton jButtonBack = new JButton("BACK");
+
+		jButtonBack.addActionListener(new ActionListener() {			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				mainFrame.setScreen(new HomeScreen(mainFrame));
+			}
+		});
 		
 		add(jButtonBack);
 	}
